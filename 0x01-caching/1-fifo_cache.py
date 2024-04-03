f6e827a88f19638214a000baab8c40f0a973e7de
omphilejmatsobe <omphilejmatsobe@gmail.com>
2024-04-04 01:14:40 +0200

    Catching System test

---
+++ 0x01-caching/1-fifo_cache.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+"""
+inherits from BaseCaching and is a caching system
+"""
+
+from base_caching import BaseCaching
+from collections import OrderedDict
+
+
+class FIFOCache(BaseCaching):
+    """
+    A class that inherits from
+    `BaseCaching` and is a caching system.
+    """
+
+    def __init__(self):
+        super().__init__()
+        self.cache_data = OrderedDict()
+
+    def put(self, key, item):
+        """
+        assign to the dictionary `self.cache_data` the
+        `item` value for the key `key`
+        """
+
+        if key is None or item is None:
+            return
+
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+            first_key, _ = self.cache_data.popitem(last=False)
+            print(f"DISCARD: {first_key}")
+
+        self.cache_data[key] = item
+
+    def get(self, key):
+        """
+        returns the value in `self.cache_data` linked to `key`
+        """
+
+        return self.cache_data.get(key, None)
