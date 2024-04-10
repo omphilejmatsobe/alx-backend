7155a4797112c464430ecc51ed0abfa965e633c4
omphilejmatsobe <omphilejmatsobe@gmail.com>
2024-04-10 23:52:32 +0200

    Html files update

---
+++ 0x02-i18n/0-app.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""
+Sestup a basic Flask app
+"""
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def index() -> str:
+    """
+    Renders an html template
+    """
+    return render_template('0-index.html')
+
+
+if __name__ == '__main__':
+    app.run()
