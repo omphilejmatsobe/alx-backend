32e0220036b34b553f1f75378d61bd86dddfbbb9
omphilejmatsobe <omphilejmatsobe@gmail.com>
2024-04-12 22:01:00 +0200

    File test

---
+++ 0x02-i18n/5-app.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python3
+"""
+Determines the best match with supported languages
+"""
+
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+
+class Config:
+    """
+    Configuration class
+    """
+
+    DEBUG = True
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+app.url_map.strict_slashes = False
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale() -> str:
+    """
+    Retrieves the locale for a web page.
+    """
+    locale = request.args.get('locale', '').strip()
+    if locale and locale in Config.LANGUAGES:
+        return locale
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
+
+def get_user(id) -> Union[Dict[str, Union[str, None]], None]:
+    """
+    Validates user login details.
+    """
+    return users.get(int(id), 0)
+
+@app.before_request
+def before_request():
+    """
+    Adds valid user to the global session object `g`
+    """
+    setattr(g, 'user', get_user(request.args.get('login_as', 0)))
+
+@app.route('/')
+def index() -> str:
+    """default route
+    """
+    return render_template("4-index.html")
+
+
+if __name__ == "__main__":
+    app.run()
